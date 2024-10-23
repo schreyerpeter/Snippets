0351554698b3cabf8eb4160f8ae3087148b6aaff
Peter Schreyer <prhschreyer@gmail.com>
2024-10-23 13:32:54 -0700

    Display formatted code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,29 @@
-import Image from "next/image";
+import Link from 'next/link';
+import { db } from '@/db';
 
-export default function Home() {
-  return <div>Home Page</div>;
+export default async function Home() {
+  const snippets = await db.snippet.findMany();
+
+  const renderedSnippets = snippets.map((snippet) => (
+    <Link
+      className="flex justify-between items-center p-2 border rounded"
+      href={`/snippets/${snippet.id}`}
+      key={snippet.id}
+    >
+      <div>{snippet.title}</div>
+      <div>View</div>
+    </Link>
+  ));
+
+  return (
+    <div>
+      <div className="flex m-2 justify-between items-center">
+        <h1 className="text-xl font-bold">Snippets</h1>
+        <Link href="/snippets/new" className="border p-2 rounded">
+          New
+        </Link>
+      </div>
+      <div className="flex flex-col gap-2">{renderedSnippets}</div>
+    </div>
+  );
 }
---
+++ src/app/snippets/[id]/page.tsx
@@ -0,0 +1,39 @@
+import { db } from '@/db';
+import { notFound } from 'next/navigation';
+
+interface SnippetShowPageProps {
+  params: {
+    id: string;
+  };
+}
+
+export default async function SnippetShowPage({
+  params,
+}: SnippetShowPageProps) {
+  await new Promise((r) => setTimeout(r, 1000));
+
+  const { id } = await params;
+
+  const snippet = await db.snippet.findFirst({
+    where: { id: parseInt(id) },
+  });
+
+  if (!snippet) {
+    return notFound();
+  }
+
+  return (
+    <div>
+      <div className="flex m-4 justify-between items-center">
+        <h1 className="text-xl font-bold">{snippet.title}</h1>
+        <div className="flex gap-4">
+          <button className="p-2 border rounded">Edit</button>
+          <button className="p-2 border rounded">Delete</button>
+        </div>
+      </div>
+      <pre className="p-3 border rounded bg-gray-200 border-gray-200">
+        <code>{snippet.code}</code>
+      </pre>
+    </div>
+  );
+}
