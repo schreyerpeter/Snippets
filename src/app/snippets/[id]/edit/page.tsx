c684542a917cae5c1b290416f1f4ece957c2b434
Peter Schreyer <prhschreyer@gmail.com>
2024-10-24 16:18:04 -0700

    Small improvement

--- src/app/snippets/[id]/edit/page.tsx
+++ src/app/snippets/[id]/edit/page.tsx
@@ -1,12 +1,22 @@
 import SnippetEditForm from '@/components/snippet-edit-form';
 import { db } from '@/db';
 import { notFound } from 'next/navigation';
+import { cache } from 'react';
 
 interface SnippetEditPageProps {
   params: {
     id: string;
   };
 }
+
+export const getSnippet = cache(async (id: string) => {
+  const snippet = await db.snippet.findFirst({
+    where: { id: parseInt(id) },
+  });
+
+  if (!snippet) return notFound();
+  return snippet;
+});
 
 export default async function SnippetEditPage({
   params,
@@ -15,13 +25,7 @@ export default async function SnippetEditPage({
 
   const { id } = await params;
 
-  const snippet = await db.snippet.findFirst({
-    where: { id: parseInt(id) },
-  });
-
-  if (!snippet) {
-    return notFound();
-  }
+  const snippet = await getSnippet(id);
 
   return (
     <div>
